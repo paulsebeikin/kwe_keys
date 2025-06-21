073102d8d52daefe63791b1f85faf2be6c1bdc9a
pauls <paulsebeikin@hotmail.com>
2025-06-21 13:50:11 +0200

    attempt 2 at getting serverless netlify functions to work

---
+++ server/.netlify/functions/api.js
@@ -0,0 +1,14 @@
+import serverless from "serverless-http";
+import express from "express";
+const authRoutes = require('../../routes/auth');
+const remoteRoutes = require('../../routes/remotes');
+const unitRoutes = require('../../routes/units');
+
+const api = express();
+
+// Routes
+api.use('/auth', authRoutes);
+api.use('/remotes', remoteRoutes);
+api.use('/units', unitRoutes);
+
+export const handler = serverless(api);
